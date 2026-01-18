53b70762064589363aedb54f9272741c39f8b567
Aakash Ohri <aakash.ohri19@gmail.com>
2026-01-18 11:59:21 +0530

    props in equitable fixed

--- lib/blocs/bloc/todolist_state.dart
+++ lib/blocs/bloc/todolist_state.dart
@@ -8,14 +8,14 @@ class TodolistState extends Equatable {
   factory TodolistState.initial() {
     return TodolistState([
       Todo(id: '1', desc: 'One', completed: false),
-      Todo(id: '2', desc: 'Two', completed: false),
-      Todo(id: '3', desc: 'Three', completed: false),
-      Todo(id: '4', desc: 'Four', completed: false),
+      // Todo(id: '2', desc: 'Two', completed: false),
+      // Todo(id: '3', desc: 'Three', completed: false),
+      // Todo(id: '4', desc: 'Four', completed: false),
     ]);
   }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [todolist];
 
   TodolistState copyWith({List<Todo>? todolist}) {
     return TodolistState(todolist ?? this.todolist);
